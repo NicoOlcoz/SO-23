704597e48695896dd08f2a1b09281fbe95cd1ccd
Fausto <143634708+faucastillo@users.noreply.github.com>
2023-10-29 17:06:09 -0300

    Update CargarArchivos.cpp

--- so-2c2023-tp1/codigo/src/CargarArchivos.cpp
+++ so-2c2023-tp1/codigo/src/CargarArchivos.cpp
@@ -42,50 +42,18 @@ void cargarMultiplesArchivos(HashMapConcurrente &hashMap,
   std::vector<std::thread> threads;
 
   std::atomic<int> siguienteIndex;
-  std::atomic_init(&siguienteIndex, -1);
-  std::mutex mutex;
+  std::atomic_init(&siguienteIndex, 0);
 
-  // auto cargarArchivoThread = [&hashMap, &filePaths, &siguienteIndex]() {
-  //   unsigned int index;
-  //   while ((index = siguienteIndex.fetch_add(1)) < filePaths.size()) {
-  //     cargarArchivo(hashMap, filePaths[index]);
-  //   }
-  // };
+  auto cargarArchivoThread = [&hashMap, &filePaths, &siguienteIndex]() {
+    int index;
+    while ((index = siguienteIndex.fetch_add(1)) < filePaths.size()) {
+      cargarArchivo(hashMap, filePaths[index]);
+    }
+  };
 
-  for (unsigned int i = 0; i < cantThreads; i++) {
-    threads.emplace_back([&hashMap, &filePaths, &siguienteIndex, &mutex]() {
-      int index;
-      while (true) {
-        mutex.lock();
-        index = siguienteIndex.fetch_add(1);
-        mutex.unlock();
-        if (index < filePaths.size()) {
-          cargarArchivo(hashMap, filePaths[index]);
-        } else {
-          break;
-        }
-      }
-    });
+  for (int i = 0; i < cantThreads; i++) {
+    threads.emplace_back(cargarArchivoThread);
   }
-
-  // std::queue<std::string> filesQueue;
-  // for (const auto &filePath : filePaths) {
-  //   filesQueue.push(filePath);
-  // }
-  // std::mutex filesQueueMutex;
-
-  // for (unsigned int i = 0; i < cantThreads; i++) {
-  //   threads.emplace_back([&hashMap, &filesQueue, &filesQueueMutex]() {
-  //     while (!filesQueue.empty()) {
-  //       filesQueueMutex.lock();
-  //       if (filesQueue.empty()) break;
-  //       auto filePath = filesQueue.front();
-  //       filesQueue.pop();
-  //       filesQueueMutex.unlock();
-  //       cargarArchivo(hashMap, filePath);
-  //     }
-  //   });
-  // }
 
   for (auto &t : threads) {
     t.join();
