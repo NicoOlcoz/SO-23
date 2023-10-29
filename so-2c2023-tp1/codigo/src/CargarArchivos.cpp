d2764752238d9ab168833d6f957cd9be34a92908
Fausto <143634708+faucastillo@users.noreply.github.com>
2023-10-28 21:06:09 -0300

    Update CargarArchivos.cpp
    No está completo

--- so-2c2023-tp1/codigo/src/CargarArchivos.cpp
+++ so-2c2023-tp1/codigo/src/CargarArchivos.cpp
@@ -1,47 +1,95 @@
 #ifndef CHM_CPP
 #define CHM_CPP
 
-#include <vector>
-#include <iostream>
-#include <fstream>
-
 #include "CargarArchivos.hpp"
 
-int cargarArchivo(
-    HashMapConcurrente &hashMap,
-    std::string filePath
-) {
-    std::fstream file;
-    int cant = 0;
-    std::string palabraActual;
+#include <atomic>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <vector>
 
-    // Abro el archivo.
-    file.open(filePath, file.in);
-    if (!file.is_open()) {
-        std::cerr << "Error al abrir el archivo '" << filePath << "'" << std::endl;
-        return -1;
-    }
-    while (file >> palabraActual) {
-        // Completar (Ejercicio 4)
-        cant++;
-    }
-    // Cierro el archivo.
-    if (!file.eof()) {
-        std::cerr << "Error al leer el archivo" << std::endl;
-        file.close();
-        return -1;
-    }
+int cargarArchivo(HashMapConcurrente &hashMap, std::string filePath) {
+  std::fstream file;
+  int cant = 0;
+  std::string palabraActual;
+
+  // Abro el archivo.
+  file.open(filePath, file.in);
+  if (!file.is_open()) {
+    std::cerr << "Error al abrir el archivo '" << filePath << "'" << std::endl;
+    return -1;
+  }
+  while (file >> palabraActual) {
+    // Completar (Ejercicio 4)
+    hashMap.incrementar(palabraActual);
+    cant++;
+  }
+  // Cierro el archivo.
+  if (!file.eof()) {
+    std::cerr << "Error al leer el archivo" << std::endl;
     file.close();
-    return cant;
+    return -1;
+  }
+  file.close();
+  return cant;
 }
 
+void cargarMultiplesArchivos(HashMapConcurrente &hashMap,
+                             unsigned int cantThreads,
+                             std::vector<std::string> filePaths) {
+  std::vector<std::thread> threads;
 
-void cargarMultiplesArchivos(
-    HashMapConcurrente &hashMap,
-    unsigned int cantThreads,
-    std::vector<std::string> filePaths
-) {
-    // Completar (Ejercicio 4)
+  std::atomic<int> siguienteIndex;
+  std::atomic_init(&siguienteIndex, -1);
+  std::mutex mutex;
+
+  // auto cargarArchivoThread = [&hashMap, &filePaths, &siguienteIndex]() {
+  //   unsigned int index;
+  //   while ((index = siguienteIndex.fetch_add(1)) < filePaths.size()) {
+  //     cargarArchivo(hashMap, filePaths[index]);
+  //   }
+  // };
+
+  for (unsigned int i = 0; i < cantThreads; i++) {
+    threads.emplace_back([&hashMap, &filePaths, &siguienteIndex, &mutex]() {
+      int index;
+      while (true) {
+        mutex.lock();
+        index = siguienteIndex.fetch_add(1);
+        mutex.unlock();
+        if (index < filePaths.size()) {
+          cargarArchivo(hashMap, filePaths[index]);
+        } else {
+          break;
+        }
+      }
+    });
+  }
+
+  // std::queue<std::string> filesQueue;
+  // for (const auto &filePath : filePaths) {
+  //   filesQueue.push(filePath);
+  // }
+  // std::mutex filesQueueMutex;
+
+  // for (unsigned int i = 0; i < cantThreads; i++) {
+  //   threads.emplace_back([&hashMap, &filesQueue, &filesQueueMutex]() {
+  //     while (!filesQueue.empty()) {
+  //       filesQueueMutex.lock();
+  //       if (filesQueue.empty()) break;
+  //       auto filePath = filesQueue.front();
+  //       filesQueue.pop();
+  //       filesQueueMutex.unlock();
+  //       cargarArchivo(hashMap, filePath);
+  //     }
+  //   });
+  // }
+
+  for (auto &t : threads) {
+    t.join();
+  }
 }
 
 #endif
